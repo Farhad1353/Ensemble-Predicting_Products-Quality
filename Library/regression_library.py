18c0beec7d15dd93906a34892b502dd03b512093
Farhad Saiepour <fsaie74@yahoo.co.uk>
2020-10-06 00:49:09 +0100

    AdaBoost

--- Library/regression_library.py
+++ Library/regression_library.py
@@ -14,6 +14,28 @@ def plot_models(X, Y, title_name, colour = 'r'):
     plt.ylabel('Accuracy %')
     plt.show()
 
+def hyper_2_param_array(param1_start, param1_end, param1_step, param2_start, param2_end, param2_step):
+    '''
+    hyper_2_param_array function creates a 2 dimension array 
+    which the rows are representing each model to be trained and 
+    the columns are representing each of the 2 HyperParameters 
+    chosen for the training
+    '''
+
+    param1_total = int((param1_end + param1_step - param1_start-1)/param1_step)
+    param2_total = int((param2_end + param2_step - param2_start-1)/param2_step)
+
+   
+    total_models = param1_total * param2_total
+    param_array = np.ones((total_models, 2), dtype=int)
+    param_array_idx = 0
+    for param1_value in range(param1_start, param1_end, param1_step):
+        for param2_value in range(param2_start, param2_end, param2_step):
+            param_array[param_array_idx, 0] = int(param1_value)
+            param_array[param_array_idx, 1] = int(param2_value)
+            param_array_idx+=1         
+    return param_array
+
 def hyper_3_param_array(param1_start, param1_end, param1_step, \
                       param2_start, param2_end, param2_step, \
                       param3_start, param3_end, param3_step):
